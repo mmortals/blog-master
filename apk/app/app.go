45317d887cb2160db2f11aa149618ab59fa08a02
wyc <wycdahaoren@163.com>
2019-12-09 22:23:30 +0800

    修改启动方式：   使用http-server封装gin 作为http服务

---
+++ apk/app/app.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// 框架实例
+type MyfApp struct {
+	Gin *gin.Engine
+}
+
+func New() (app *MyfApp, err error) {
+
+	// 创建APP
+	app = &MyfApp{}
+
+	// 创建Gin
+	app.initGin()
+	// 创建客户端
+
+	// 注册中间件
+	return
+}
+
+// 初始化Gin
+func (app *MyfApp) initGin() {
+
+	// 调试模式
+
+	// 生产模式
+	app.Gin = gin.New()
+}
+
+// 启动APP
+func (app *MyfApp) Run() (err error) {
+
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		Handler:      app.Gin,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	//启动server服务
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errMsg := fmt.Sprintf("启动server失败：%+v, %+v ", srv, err)
+			panic(errMsg)
+		}
+	}()
+
+	//等待优雅退出命令
+	app.waitGraceExit(srv)
+	return
+
+}
+
+// 等待优雅退出
+func (app *MyfApp) waitGraceExit(server *http.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			fmt.Fprintf(os.Stdout, "收到信号: %s, 服务正在退出... \n", s.String())
+			server.Close()
+			return
+		case syscall.SIGHUP:
+		default:
+		}
+	}
+}
